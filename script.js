041f293a0ff932f1e309944ad1ec76f0125e5b4e
liamofnazareth <liamweilbakerallen@gmail.com>
2024-06-17 10:38:07 -0400

    project finished, may come back to polish

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const container = document.getElementById("container");
 const button = document.getElementById("button");
-var height = 1000;
+var height;
 const grid = [];
 const stylesheet = document.styleSheets[0];
 
 function generateGrid(input) {
     height = 1000 / input;
     for (let i = 0; i < input * input; i++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-        stylesheet.cssRules[0].style.height = height;
-        stylesheet.cssRules[1].style.width = height;
-        container.appendChild(box);
+        grid[i] = document.createElement("div");
+        grid[i].style.cssText = "height: " + height + "px;" + "width: " + height + "px;" + "background-color: lightgrey;";
+        container.appendChild(grid[i]);
+        grid[i].addEventListener("mouseover", function () {
+            changeColor(i)
+        });
     }
 }
 
+function changeColor(index) {
+    grid[index].style.backgroundColor = "black";
+}
 button.addEventListener("click", function () {
+    for (let j = 0; j < grid.length; j++) {
+        grid[j].remove();
+    }
+    grid.length = 0;
     generateGrid(prompt("enter number"));
 });
 
